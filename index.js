046f037240f9a89d42630bf80738768a9222c022
Jonny Howle <jonny.howle@gmail.com>
2022-04-10 14:30:38 -0600

    commit tree 9ea465907838aa0256789a50018438f0f43cd12e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+/* function findResult(element, index, array){
+    if(element.result === "W"){
+    return element.year
+}}
+
+
+function superbowlWin(array) {
+    const winningYear = array.find(findResult);
+    return winningYear;
+
+
+} */
+
+
+   
+superbowlWin = (record) => {
+    const result = record.find( record => record.result === "W" );
+    return !!result ? result.year : undefined;
+  }
